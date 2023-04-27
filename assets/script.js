a035237109c0891cedd9966f5e790ff69978309b
StefanoGalandrini <stefe68@gmail.com>
2023-04-27 18:21:16 +0200

    Add condition to stop the timer at endTime

--- assets/script.js
+++ assets/script.js
@@ -3,22 +3,45 @@ let hoursLeft = document.querySelector("#hours");
 let minutesLeft = document.querySelector("#minutes");
 let secondsLeft = document.querySelector("#seconds");
 
-const endTime = "2023-04-28T09:30:00";
+// set final date and time
+const endTime = "2023-04-27T18:20:00";
 
+// clear and reset DOM innerHTML
 hoursLeft.innerHTML = "";
 minutesLeft.innerHTML = "";
 secondsLeft.innerHTML = "";
 
+// check time every second and update DOM innerHTML
 const timer = setInterval(() => {
+	// function that returns an array with remaining time
 	const timeRemaining = getRemaining(endTime);
 
-	console.log(timeRemaining);
+	// inject array values in DOM elements
 	hoursLeft.innerHTML = `${timeRemaining[0]}`;
 	minutesLeft.innerHTML = `${timeRemaining[1]}`;
 	secondsLeft.innerHTML = `${timeRemaining[2]}`;
+
+	// condition to stop the timer when it gets to endTime
+	if (
+		timeRemaining[0] <= 0 &&
+		timeRemaining[1] <= 0 &&
+		timeRemaining[2] <= 0
+	) {
+		clearInterval(timer);
+	}
 }, 1000);
 
-// FUNCTIONS DEFINITION
+/*
+FUNCTIONS DEFINITION
+*/
+
+// - receives the final date and time (endTime)
+// - gets actual date and time
+// - calculates the difference in milliseconds
+// - transforms millisenconds in hours, minutes and seconds
+// - converts numbers to 2-charachters strings eventually adding prefix "0"
+// - returns an array with ["hh", "mm", "ss"]
+
 function getRemaining(endTime) {
 	const start = new Date().getTime();
 	const end = new Date(endTime).getTime();
