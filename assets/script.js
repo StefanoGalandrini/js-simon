da64962725526e3b72190ff30c3c78c370a099fe
StefanoGalandrini <stefe68@gmail.com>
2023-04-27 19:16:08 +0200

    Add function changing nums color (actually useless)

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ let minutesLeft = document.querySelector("#minutes");
 let secondsLeft = document.querySelector("#seconds");
 
 // set final date and time
-const endTime = "2023-04-27T18:20:00";
+const endTime = "2023-04-28T09:30:00";
 
 // clear and reset DOM innerHTML
 hoursLeft.innerHTML = "";
@@ -16,10 +16,21 @@ const timer = setInterval(() => {
 	// function that returns an array with remaining time
 	const timeRemaining = getRemaining(endTime);
 
+	const color = textColor(
+		timeRemaining[0],
+		timeRemaining[1],
+		timeRemaining[2],
+	);
+
 	// inject array values in DOM elements
 	hoursLeft.innerHTML = `${timeRemaining[0]}`;
 	minutesLeft.innerHTML = `${timeRemaining[1]}`;
 	secondsLeft.innerHTML = `${timeRemaining[2]}`;
+
+	// gradually changes color to white
+	hoursLeft.style.color = color[0];
+	minutesLeft.style.color = color[1];
+	secondsLeft.style.color = color[2];
 
 	// condition to stop the timer when it gets to endTime
 	if (
@@ -53,4 +64,17 @@ function getRemaining(endTime) {
 	const numToString = (time) => time.toString().padStart(2, "0");
 
 	return [numToString(hours), numToString(minutes), numToString(seconds)];
+}
+
+// function to gradually change numbers color to white
+// actually useless but just for fun
+function textColor(h, m, s) {
+	const hCol = (255 - (220 / 24) * h).toString();
+	const mCol = (255 - (220 / 60) * m).toString();
+	const sCol = (255 - (220 / 60) * s).toString();
+
+	const hourColor = `rgb(${hCol * 1.5}, ${hCol * 1.5}, ${hCol})`;
+	const minColor = `rgb(${mCol * 1.5}, ${mCol * 1.5}, ${mCol})`;
+	const secColor = `rgb(${sCol * 1.5}, ${sCol * 1.5}, ${sCol})`;
+	return [hourColor, minColor, secColor];
 }
